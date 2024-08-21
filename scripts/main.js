1ecc40ad60b61ea4b1138657c7530bc8a997fd8f
t-tiktok <ilya81583@gmail.com>
2024-08-21 18:40:12 +0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,30 +1,32 @@
-const constraints = { video: true }
+const constraints = { video: true };
 
-const stream = await navigator.mediaDevices.getUserMedia(constraints)
+const stream = await navigator.mediaDevices.getUserMedia(constraints);
 
-const videoTracks = stream.getVideoTracks()
+const videoTracks = stream.getVideoTracks();
 
-const videoTrack = stream.getVideoTracks()[0]
+const videoTrack = stream.getVideoTracks()[0];
 
-const trackCapabilities = videoTrack.getCapabilities()
+const trackCapabilities = videoTrack.getCapabilities();
 
-const trackSettings = videoTrack.getSettings()
+const trackSettings = videoTrack.getSettings();
 
 const advancedConstraints = {
     contrast: 75,
     sharpness: 75
-   }
-   // https://w3c.github.io/mediacapture-main/#dom-mediastreamtrack-applyconstraints
-await applyConstraints({ advanced: [advancedConstraints] })
+};
 
-const imageCapture = new ImageCapture(videoTrack)
+// Apply advanced constraints
+await videoTrack.applyConstraints({ advanced: [advancedConstraints] });
 
-const photoCapabilities = await imageCapture.getPhotoCapabilities()
-// https://www.w3.org/TR/image-capture/#dom-imagecapture-getphotosettings
-const photoSettings = await imageCapture.getPhotoSettings()
+const imageCapture = new ImageCapture(videoTrack);
 
-const blob = await imageCapture.takePhoto(photoSettings)
+const photoCapabilities = await imageCapture.getPhotoCapabilities();
 
+const photoSettings = await imageCapture.getPhotoSettings();
+
+const blob = await imageCapture.takePhoto(photoSettings);
+
+// Create a temporary URL for the blob
 const url = URL.createObjectURL(blob);
 
 // Display the image in a new window or tab
